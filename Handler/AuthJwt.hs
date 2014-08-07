0adec317e961f477484b34091d9484ad5bd08f98
Carlo Hamalainen <carlo@carlo-hamalainen.net>
2014-08-07 14:18:39 +0200

    Using hs-jose and hw-jwt libraries.
    https://github.com/frasertweedale/hs-jose
    
    https://github.com/frasertweedale/hs-jwt

--- Handler/AuthJwt.hs
+++ Handler/AuthJwt.hs
@@ -8,13 +8,39 @@ import Control.Monad (join, when, mzero)
 import Data.Maybe
 import Data.Aeson
 
-import qualified Web.JWT as JWT
+-- import qualified Web.JWT as JWT
 
 import qualified Data.Map as Map
 import qualified Data.Text as T
 import qualified Data.ByteString.Lazy as BSL
 
 import Data.Text.Encoding
+
+import qualified Data.Text as T
+import qualified Crypto.JWT as JWT
+
+import qualified Crypto.JOSE as JOSE
+import qualified Data.HashMap.Lazy as HM
+
+import Crypto.JOSE.JWA.JWK
+import Crypto.JOSE.Types
+
+import Data.Aeson
+import Control.Applicative ((<$>), (<*>))
+import Control.Monad (mzero)
+
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Lazy as BL
+import qualified Data.ByteString.Char8 as BL8
+
+-- for string to char8
+import Data.ByteString.Internal (unpackBytes)
+import Data.ByteString.Char8 (pack)
+import GHC.Word (Word8)
+
+getRight :: Either a b -> b
+getRight (Right b) = b
+getRight _ = error "derp"
 
 configIss :: T.Text
 configIss = "https://rapid.test.aaf.edu.au" -- or maybe "https://rapid.aaf.edu.au"
@@ -51,32 +77,49 @@ instance FromJSON AAFAttributes where
 
     parseJSON _          = mzero
 
+strToWord8s :: String -> [Word8]
+strToWord8s = unpackBytes . pack
+
 postAuthJwtR :: Handler ()
 postAuthJwtR = do
     jwtInput <- lookupPostParam "assertion"
 
     when (isNothing jwtInput) $ permissionDenied "No assertion found in POST."
 
-    let decodedAndVerified  = join $ JWT.decodeAndVerifySignature (JWT.secret secret) <$> jwtInput
-        claimset            = JWT.claims <$> decodedAndVerified
-        audience            = join $ JWT.aud <$> claimset
-        attributes          = join $ Map.lookup "https://aaf.edu.au/attributes" <$> JWT.unregisteredClaims <$> claimset
-        iss                 = join $ JWT.iss <$> claimset
+    let
+        jwtInput' = BL.pack $ strToWord8s $ T.unpack $ fromJust jwtInput
+        j = JOSE.decodeCompact jwtInput' :: Either JOSE.Error JWT.JWT
+        jwt = getRight j
 
-        -- In a production system, store the jti
-        -- values to guard against a replay attack.
-        jti                 = join $ JWT.jti <$> claimset
+        jwk :: JOSE.JWK
+        jwk = JOSE.JWK k z z z z z z z z
+            where z = Nothing
+                  k = OctKeyMaterial $ OctKeyParameters Oct $ Base64Octets (BL8.pack $ T.unpack secret)
 
-    when (audience /= JWT.stringOrURI configAudience) $
-        permissionDenied "Not for this audience."
+        -- Things that I have to look up:
+        claimset = JWT.jwtClaimsSet jwt
+        iss      = JWT.claimIss claimset
+        audience = JWT.claimAud claimset
 
-    when (iss /= JWT.stringOrURI configIss) $
+        -- In the unregistered claims I will find things about
+        -- the user's institutional email, staff/student affiliation, etc.
+        unregisteredClaims = JWT.unregisteredClaims claimset
+        attributes         = HM.lookup "https://aaf.edu.au/attributes" unregisteredClaims
+
+        -- I guess that jwk should be a function of the secret?
+        isvalid = JWT.validateJWSJWT jwk jwt
+
+    when (audience /= (Just $ JWT.Special $ JWT.OrURI $ URI $ fromJust $ parseURI $ T.unpack configAudience)) $
+        permissionDenied $ "Not for this audience: " -- ++ show audience
+
+    when (iss /= (Just $ JWT.OrURI $ URI $ fromJust $ parseURI $ T.unpack configIss)) $
          permissionDenied "Issuer does not match."
 
-    when (isNothing claimset) $
-        permissionDenied "Could not decode claimset."
+    when (isNothing attributes) $
+        permissionDenied "Could not lookup AAF attributes."
 
-    -- JWT looks good. Store the attributes in the session.
+    when (not isvalid) $
+        permissionDenied "Could not validate JWT."
 
     case (fromJSON $ fromJust attributes :: Result AAFAttributes) of
         Success attributes' -> do
@@ -86,6 +129,6 @@ postAuthJwtR = do
         Error e             -> permissionDenied $ "Error: JSON error when decoding attributes: " `T.append` (T.pack e)
 
     -- Set claimset. Beware that decodeUtf8 can explode; use decodeUtf8' or similar.
-    setSession "jwt" (decodeUtf8 $ BSL.toStrict $ encode $ fromJust claimset)
+    setSession "jwt" (decodeUtf8 $ BSL.toStrict $ encode claimset)
 
     redirect WelcomeR
--- build_in_sandbox.sh
+++ build_in_sandbox.sh
@@ -8,5 +8,8 @@ rm -fr .cabal-sandbox cabal.sandbox.config dist
 
 cabal sandbox init
 
+cabal sandbox add-source ../hs-jose
+cabal sandbox add-source ../hs-jwt
+
 cabal install --haddock-hyperlink-source --dependencies-only
 cabal install --haddock-hyperlink-source
