ad8d8b16217b3a72c02d218c4d09dfd58e4c0fb6
Carlo Hamalainen <carlo@carlo-hamalainen.net>
2014-08-11 14:34:35 +0200

    Remove add-source of hs-jwt as hs-jose now contains jwt stuff.

--- build_in_sandbox.sh
+++ build_in_sandbox.sh
@@ -9,7 +9,6 @@ rm -fr .cabal-sandbox cabal.sandbox.config dist
 cabal sandbox init
 
 cabal sandbox add-source ../hs-jose
-cabal sandbox add-source ../hs-jwt
 
 cabal install --haddock-hyperlink-source --dependencies-only
 cabal install --haddock-hyperlink-source
